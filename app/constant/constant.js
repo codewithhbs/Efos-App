1e62e8bdc6812b0ff10c62e91b66b0df9d3bfd4f
Sac6289 <sachinkumsr59@gmail.com>
2026-07-16 13:37:07 +0530

    fix: comment out unused API URL for clarity

--- app/constant/constant.js
+++ app/constant/constant.js
@@ -1,4 +1,7 @@
 export const API_URL='https://api.epinfoways.com/api/v1'
 // export const API_URL='http://192.168.1.23:9860/api/v1'
+// export const API_URL='http://77.37.44.187:9860/api/v1' server connect
+
+
 
 
